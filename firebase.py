d314c9a80f5ee2a5184d5d72f10a27daf55dce5e
TarangGarlapally <taranggarlapally@gmail.com>
2021-06-26 18:09:00 +0530

    moved firebase config to env

--- firebase.py
+++ firebase.py
@@ -1,15 +1,18 @@
 import pyrebase
+from dotenv import dotenv_values
+
+env = dotenv_values(".env")
 
 def init():
 	firebaseConfig = {
-	    "apiKey": "AIzaSyAwP_J2UEIX7gO2fIL1Ovx1fDcR-g5QVDg",
-	    "authDomain": "pegasuschat.firebaseapp.com",
-	    "projectId": "pegasuschat",
-	    "storageBucket": "pegasuschat.appspot.com",
-	    "messagingSenderId": "1084666068688",
-	    "appId": "1:1084666068688:web:4505625e8d5f298a8728d5",
-	    "measurementId": "G-MRP0J0SQLZ",
-	    "databaseURL": "https://pegasuschat-default-rtdb.firebaseio.com"
+	    "apiKey": env["apiKey"],
+	    "authDomain": env["authDomain"],
+	    "projectId": env["projectId"],
+	    "storageBucket": env["storageBucket"],
+	    "messagingSenderId": env["messagingSenderId"],
+	    "appId": env["appId"],
+	    "measurementId": env["measurementId"],
+	    "databaseURL": env["databaseURL"]
 	}
 
 	return pyrebase.initialize_app(firebaseConfig)
